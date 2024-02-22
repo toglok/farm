8f200b5700bbd9894d1b3937c99f8336b0311f59
toglok <52659369+toglok@users.noreply.github.com>
2024-02-22 23:17:56 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// File: index.js
+
+// Fungsi untuk menampilkan pesan sapaan
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+// Contoh penggunaan fungsi greet
+console.log(greet("John")); // Output: Hello, John!
